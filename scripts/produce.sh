50ed0593a2a9b11d89bf2ce4e884a17ae4d4700c
Kumaran Rajendhiran <kumaran@airt.ai>
2023-09-15 15:40:32 +0530

    Use container_name

--- scripts/produce.sh
+++ scripts/produce.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker exec -it faststream-gen-template-kafka-1 /bin/bash -c 'echo \{\"a\":3,\"b\":5\} | /opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list kafka:9092 --topic multiply'
+docker exec -it bitnami_kafka /bin/bash -c 'echo \{\"a\":3,\"b\":5\} | /opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list kafka:9092 --topic multiply'
 
-docker exec -it faststream-gen-template-kafka-1 /bin/bash -c 'echo \{\"a\":4,\"b\":6\} | /opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list kafka:9092 --topic multiply'
+docker exec -it bitnami_kafka /bin/bash -c 'echo \{\"a\":4,\"b\":6\} | /opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list kafka:9092 --topic multiply'
 
-docker exec -it faststream-gen-template-kafka-1 /bin/bash -c 'echo \{\"a\":5,\"b\":7\} | /opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list kafka:9092 --topic multiply'
+docker exec -it bitnami_kafka /bin/bash -c 'echo \{\"a\":5,\"b\":7\} | /opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list kafka:9092 --topic multiply'
--- scripts/subscribe.sh
+++ scripts/subscribe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it faststream-gen-template-kafka-1 /opt/bitnami/kafka/bin/kafka-console-consumer.sh --consumer.config /opt/bitnami/kafka/config/consumer.properties --bootstrap-server kafka:9092 --topic results --from-beginning
+docker exec -it bitnami_kafka /opt/bitnami/kafka/bin/kafka-console-consumer.sh --consumer.config /opt/bitnami/kafka/config/consumer.properties --bootstrap-server kafka:9092 --topic results --from-beginning
